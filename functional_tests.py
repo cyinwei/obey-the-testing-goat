c45cc3a7426f0fa4a10db44fb7c4f1d2ce01f34f
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2018-08-31 14:12:43 -0400

    Chapter 4 (finish) | Template has header, input, table skeleton.
    Functional tests is further along to where the user is waiting for the to do
    list to update.

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('todo-list')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Become governor of California' for row in rows)
+            any(row.text == '1: Become governor of California' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There's a still input following the item.  Kobe then enters
