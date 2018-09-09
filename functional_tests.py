ad31d1739c4c322f1c18e3916ed84f773af79cae
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2018-09-09 10:34:10 -0400

    Chapter 5 | Refactor functional_tests.py to function-ify repeats.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,19 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_task_in_todo_list(self, task_text):
+        # re-get the table, since the page will probably have refreshed
+        table = self.browser.find_element_by_id('id-todo-list')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(task_text, [row.text for row in rows])
+
+    def submit_todo_task(self, task_text):
+        # re-get the inputbox, since the page might have refreshed
+        inputbox = self.browser.find_element_by_id('id-new-todo-item-input')
+        inputbox.send_keys(task_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)  # wait for page to refresh
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Kobe has a heard of the awesome ToDo app, superlists.  He visits
         # the homepage.
@@ -26,49 +39,27 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter the vision you wish to bring to reality')
-        # He types "Become governor of California" into a text box as a task
-        inputbox.send_keys('Become governor of California')
 
-        # He submits the task
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        # He types "Become governor of California" into a text box as a task
+        # and submits it by pressing ENTER
+        self.submit_todo_task('Become governor of California')
 
         # After submitting, he sees an updated ToDo list that looks like
         # "1: Become governor of California" as an item in a to-do list
-        table = self.browser.find_element_by_id('id-todo-list')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1. Become governor of California', [row.text for row in rows],
-            (f"New to-do item did not appear in table.  "
-             f"List contents were: \n{table.text}")
-        )
+        self.check_for_task_in_todo_list('1. Become governor of California')
 
         # Kobe then enters "Become president of the United States", as that is
         # his next task.
-
-        # get the inputbox after the site is refreshed
-        inputbox = self.browser.find_element_by_id('id-new-todo-item-input')
-        inputbox.send_keys('Become president of the United States')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.submit_todo_task('Become president of the United States')
 
         # He sees the new entry on the ToDo list after the page refreshes
-
-        # get the table again after the site is refreshed
-        table = self.browser.find_element_by_id('id-todo-list')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2. Become president of the United States',
-            [row.text for row in rows],
-            (f'Second to-do item did not appear in table.  '
-             f'List contents were: \n{table.text}')
-        )
-
-        self.fail('Finish the functional tests!')
+        self.check_for_task_in_todo_list(
+            '2. Become president of the United States')
 
         # Kobe wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the functional tests!')
 
         # Kobe checks that url just to be safe.  His ToDo list is still there.
 
