841cefd82177b0a93defd8b1635e9c0969aa4116
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2018-09-10 23:52:14 -0400

    Chapter 6 (Finish) | Add smarter retries to replace voodoo waits

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,13 @@
 import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(LiveServerTestCase):
+    MAX_WAITS = 10
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -12,17 +15,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def check_for_task_in_todo_list(self, task_text):
-        # re-get the table, since the page will probably have refreshed
-        table = self.browser.find_element_by_id('id-todo-list')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(task_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id-todo-list')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(task_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAITS:
+                    raise e
+                time.sleep(0.5)
 
     def submit_todo_task(self, task_text):
         # re-get the inputbox, since the page might have refreshed
         inputbox = self.browser.find_element_by_id('id-new-todo-item-input')
         inputbox.send_keys(task_text)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)  # wait for page to refresh
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Kobe has a heard of the awesome ToDo app, superlists.  He visits
@@ -51,7 +60,6 @@ class NewVisitorTest(LiveServerTestCase):
         # Kobe then enters "Become president of the United States", as that is
         # his next task.
         self.submit_todo_task('Become president of the United States')
-
 
         # He sees the new entry on the ToDo list after the page refreshes
         self.check_for_task_in_todo_list(
