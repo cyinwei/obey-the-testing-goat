943a151819aef5c63f374475fde7daff00d2b60a
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2018-09-10 23:41:58 -0400

    Chapter 6 | Move functional tests to use django.test.LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
-import unittest
 import time
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Kobe has a heard of the awesome ToDo app, superlists.  He visits
         # the homepage.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # He looks as the title and confirms he's on the right URL
         self.assertIn('Awesome To-Do Lists', self.browser.title)
@@ -65,7 +65,3 @@ class NewVisitorTest(unittest.TestCase):
         # Kobe checks that url just to be safe.  His ToDo list is still there.
 
         # Satisfied, he starts implementing his ToDo list
-
-
-if __name__ == '__main__':
-    unittest.main()
