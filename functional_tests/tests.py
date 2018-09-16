7b22dea07f1e1387ff52f84a6f465d38a675b7ba
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2018-09-16 13:30:12 -0400

    Chapter 8 | Add Bootstrap4 for styling.
    Passed the inputbox centered functional test.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     MAX_WAITS = 10
 
     def setUp(self):
